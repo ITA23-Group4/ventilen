42b3f21123ec023f6c947dd985ebae95c0d9b5ad
Nikolaj <dknikolajk@gmail.com>
2024-04-18 15:19:21 +0200

    Made layout on WelcomeScreen.kt

--- app/src/main/java/com/example/ventilen_app/ui/screens/Welcome/WelcomeScreen.kt
+++ app/src/main/java/com/example/ventilen_app/ui/screens/Welcome/WelcomeScreen.kt
@@ -1,20 +1,68 @@
 package com.example.ventilen_app.ui.screens.Welcome
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.ventilen_app.R
 import com.example.ventilen_app.ui.composables.FilledButton
+import com.example.ventilen_app.ui.theme.Mocca
+import com.example.ventilen_app.ui.theme.Orange
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 @Composable
 fun WelcomeScreen(
     onClick: () -> Unit
 ) {
-    Text(text = "Hello")
 
-    Column {
-        FilledButton(text = "Login", onClick = onClick, color = Color.Red)
-        FilledButton(text = "Tilmeld", onClick = onClick, color = Color.Red)
+    Column(
+        Modifier.background(Mocca)
+    ) {
+        Spacer(modifier = Modifier.height(110.dp))
+        Image(
+            painter = painterResource(id = R.drawable.vent_logo), // Replace with your image resource ID
+            contentDescription = "Welcome Image",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .width(166.dp),
+            contentScale = ContentScale.FillWidth,
+        )
+        Spacer(modifier = Modifier.height(66.dp))
+        Text(text = "Velcommen",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(14.dp))
+        Text(text = "log in eller tilmeld dig",
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+        )
+        Spacer(modifier = Modifier.height(28.dp))
+        FilledButton(
+            text = "Login",
+            onClick = onClick,
+            color = Orange,
+            padding = PaddingValues(horizontal = 25.dp, vertical = 0.dp)
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+        FilledButton(
+            text = "Tilmeld",
+            onClick = onClick,
+            color = Orange,
+            padding = PaddingValues(horizontal = 25.dp, vertical = 0.dp))
     }
 }
