3eb60eb5eac73237aa25a9cd830c37d953a8deb9
CBT MacBook <Christianbt96@gmail.com>
2024-04-18 16:48:29 +0200

    Made customizable outlined button composable.

--- app/src/main/java/com/example/ventilen_app/ui/screens/Username/UsernameScreen.kt
+++ app/src/main/java/com/example/ventilen_app/ui/screens/Username/UsernameScreen.kt
@@ -2,9 +2,11 @@ package com.example.ventilen_app.ui.screens.Username
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import com.example.ventilen_app.ui.composables.CustomOutlinedButton
 import com.example.ventilen_app.ui.composables.CustomTextField
 import com.example.ventilen_app.ui.composables.FilledButton
 
@@ -20,5 +22,6 @@ fun UsernameScreen(
     Column {
         CustomTextField(text = text, onValueChange = {onValueChange(it)} )
         FilledButton(text = "Fortsæt", onClick = onClick, color = Color.Red)
-    }
+        CustomOutlinedButton(text = "Back", onClick = { /*TODO*/ }, borderColor = Color.Red, textColor = Color.Black )
+        }
 }
