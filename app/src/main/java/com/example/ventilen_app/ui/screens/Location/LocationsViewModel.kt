25a51c8f5bb50580e974964f5ba3b9f69201e1b4
CBT MacBook <Christianbt96@gmail.com>
2024-05-05 16:30:21 +0200

    Manual linting

--- app/src/main/java/com/example/ventilen_app/ui/screens/Location/LocationsViewModel.kt
+++ app/src/main/java/com/example/ventilen_app/ui/screens/Location/LocationsViewModel.kt
@@ -1,25 +1,17 @@
 package com.example.ventilen_app.ui.screens.Location
 
 import android.util.Log
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ventilen_app.data.Repository
-import com.example.ventilen_app.data.models.Location
 import kotlinx.coroutines.launch
 
 class LocationsViewModel: ViewModel() {
-
     private val repository: Repository = Repository()
     var locationNames: List<String> = emptyList()
-
-
     init {
         getLocations()
     }
-
     private fun getLocations() {
         viewModelScope.launch {
             try {
