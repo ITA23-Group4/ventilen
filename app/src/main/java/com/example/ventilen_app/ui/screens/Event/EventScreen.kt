450aa1bb77788463b72b3c6799a421ec73e2a928
dknik <dknikolajk@gmail.com>
2024-05-06 09:52:49 +0200

    remove later

--- app/src/main/java/com/example/ventilen_app/ui/screens/Event/EventScreen.kt
+++ app/src/main/java/com/example/ventilen_app/ui/screens/Event/EventScreen.kt
@@ -35,6 +35,7 @@ fun EventScreen(
                     attendeesAmount = event.attendeesByUID.size,
                     onAttend = { onAttend(event.id!!) },
                     onNotAttend = { onNotAttend(event.id!!) }
+                    //Lol
                 )
             }
         }
