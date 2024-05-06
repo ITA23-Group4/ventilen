f6ecf286726ef86e3c6d8c22bbe76b7402f3c3e7
marcus_rk <makj0005@stud.kea.dk>
2024-05-06 16:44:30 +0200

    Added Comparable interface and added compareTo function in Event data class

--- app/src/main/java/com/example/ventilen_app/data/models/Event.kt
+++ app/src/main/java/com/example/ventilen_app/data/models/Event.kt
@@ -8,8 +8,22 @@ data class Event(
     val title: String = "",
     val attendeesByUID: MutableList<String> = mutableListOf(),
     @DocumentId val id: String,
-) {
+) : Comparable<Event> {
     fun withUpdatedAttendees(attendees: MutableList<String>): Event {
         return this.copy(attendeesByUID = attendees)
     }
+
+    /**
+     * Compares this Event with another Event based on their names.
+     *
+     * @param other The other Event to compare with.
+     * @return A negative integer if this name is less than [other]'s name,
+     *         zero if they are equal, or a positive integer if this name is greater.
+     *
+     * TODO: Look into Comparator if we want to sort in other ways than natural order
+     */
+    override fun compareTo(other: Event): Int {
+        return title.compareTo(other.title)
+    }
+
 }
