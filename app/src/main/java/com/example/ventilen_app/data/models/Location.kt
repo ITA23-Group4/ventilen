3ee8269bec7c8588b8e80d03bb817b397c5730f4
marcus_rk <makj0005@stud.kea.dk>
2024-05-06 16:18:45 +0200

    Added Comparable interface and added compareTo function in Location data class

--- app/src/main/java/com/example/ventilen_app/data/models/Location.kt
+++ app/src/main/java/com/example/ventilen_app/data/models/Location.kt
@@ -5,4 +5,16 @@ import com.google.firebase.firestore.DocumentId
 data class Location(
     val name: String,
     @DocumentId var uid: String? = null
-)
+) : Comparable<Location> {
+
+    /**
+     * Compares this Location with another Location based on their names.
+     *
+     * @param other The other Location to compare with.
+     * @return A negative integer if this name is less than [other]'s name,
+     *         zero if they are equal, or a positive integer if this name is greater.
+     */
+    override fun compareTo(other: Location): Int {
+        return name.compareTo(other.name)
+    }
+}
