3d52337f7378c4038451a7ba7625a57c3f4faa3a
marcus_rk <makj0005@stud.kea.dk>
2024-05-08 11:15:46 +0200

    Delted CustomEventCardViewModel.kt and started working on CustomChatCard

--- app/src/main/java/com/example/ventilen_app/ui/components/CustomChatCardComponent/CustomChatCard.kt
+++ app/src/main/java/com/example/ventilen_app/ui/components/CustomChatCardComponent/CustomChatCard.kt
@@ -1,16 +1,35 @@
 package com.example.ventilen_app.ui.components.CustomChatCardComponent
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomChatCard(
-
+    locationName: String,
+    latestMessage: String,
+    abbreviation: String
 ) {
     Card(
-
+        modifier = Modifier
+            .fillMaxWidth(),
+        onClick = { }
     ){
-
+        Row (
+            modifier = Modifier.fillMaxWidth()
+        ){
+            Text(text = abbreviation)
+            Column {
+                Text(text = locationName)
+                Text(text = latestMessage)
+            }
+        }
     }
 }
