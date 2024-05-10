98f6a7b412787991f40e08cc43a616f1f566f399
marcus_rk <makj0005@stud.kea.dk>
2024-05-10 12:26:17 +0200

    Added containerColor to TopAppBar in AuthScaffold

--- app/src/main/java/com/example/ventilen_app/ui/components/scaffold/AuthScaffold.kt
+++ app/src/main/java/com/example/ventilen_app/ui/components/scaffold/AuthScaffold.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.rounded.KeyboardArrowLeft
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.navigation.NavController
@@ -27,7 +28,9 @@ fun AuthScaffold(
     Scaffold(
         topBar = {
             TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(),
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.secondary
+                ),
                 navigationIcon = {
                     if (showBackButton) {
                         CustomBackButton(onNavigateBack = onNavigateBack)
